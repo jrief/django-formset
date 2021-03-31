798e5ee3371331c7d9ddd9cf6ad3239350ca8ecd
Jacob Rief <jacob.rief@uibk.ac.at>
2021-03-31 09:51:42 +0200

    rename bootstrap4 -> bootstrap

---
+++ formset/mixins/bootstrap.py
@@ -0,0 +1,59 @@
+import copy
+
+from django.utils.html import format_html
+
+from formset.mixins.default import FormMixin, WidgetMixin
+
+
+class BootstrapWidgetMixin(WidgetMixin):
+    template_mapping = {
+        'RadioSelect': 'formset/widgets/bootstrap/radio.html',
+        'CheckboxInput': 'formset/widgets/bootstrap/checkbox.html',
+        'CheckboxSelectMultiple': 'formset/widgets/bootstrap/checkboxselectmultiple.html',
+    }
+
+
+class BootstrapFormMixin(FormMixin):
+    field_css_classes = 'form-group'
+    help_text_html='<span class="form-text text-muted">%s</span>'
+    widget_css_classes = {
+        'text': 'form-control',
+        'email': 'form-control',
+        'date': 'form-control',
+        'select': 'form-control',
+        'selectmultiple': 'form-control',
+        'number': 'form-control',
+        'textarea': 'form-control',
+        'password': 'form-control',
+    }
+
+    def render_label(self, bound_field, contents, attrs, label_suffix):
+        label_css_classes = getattr(self, 'label_css_classes', None)
+        if bound_field.widget_type == 'checkbox':
+            if label_css_classes:
+                label_tag = format_html('<div class="{}"></div>', label_css_classes)
+            else:
+                label_tag = ''
+        else:
+            if label_css_classes:
+                attrs = dict(attrs or {})
+                if 'class' in attrs:
+                    attrs['class'] += ' ' + label_css_classes
+                else:
+                    attrs['class'] = label_css_classes
+            label_tag = super().render_label(bound_field, contents, attrs, label_suffix)
+        return label_tag
+
+    def get_widget_attrs(self, bound_field, attrs, widget):
+        attrs = super().get_widget_attrs(bound_field, attrs, widget)
+
+        # checkbox widgets render their label themselves
+        if bound_field.widget_type == 'checkbox':
+            attrs['checkbox_label'] = bound_field.label
+
+        return attrs
+
+    def get_widget(self, field):
+        widget = copy.deepcopy(field.widget)
+        widget.__class__ = type(widget.__class__.__name__, (BootstrapWidgetMixin, widget.__class__), {})
+        return widget
