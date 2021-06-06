b35b4387566ff378d99e9cce2568f9ad7a8856ff
Jacob Rief <jacob.rief@uibk.ac.at>
2021-06-06 21:41:47 +0200

    use Django’s internal prefix as <form name=“…”>

--- testapp/forms.py
+++ testapp/forms.py
@@ -15,7 +15,7 @@ def validate_password(value):
 
 
 class SubscribeForm(forms.Form):
-    name = 'subscribe'
+    prefix = 'subscribe'
 
     CONTINENT_CHOICES = [
         ('', "––– please select –––"), ('am', "America"), ('eu', "Europe"), ('as', "Asia"),
@@ -200,7 +200,7 @@ class UploadForm(forms.Form):
 
 
 class PersonaForm(forms.Form):
-    name = 'persona'
+    prefix = 'persona'
 
     first_name = fields.RegexField(
         r'^[A-Z][a-z -]+$',
