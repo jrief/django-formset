5f201c140727cb139e8fcabf48fd4a1bb88465fe
Jacob Rief <jacob.rief@uibk.ac.at>
2021-10-06 00:45:02 +0200

    Bootstrap renders without visible errors

--- formset/renderers/bootstrap.py
+++ formset/renderers/bootstrap.py
@@ -4,10 +4,10 @@ from formset.renderers.default import FormRenderer as DefaultFormRenderer
 class FormRenderer(DefaultFormRenderer):
     _template_mapping = dict(DefaultFormRenderer._template_mapping, **{
         'django/forms/default.html': 'formset/bootstrap/form.html',
-        'django/forms/widgets/radio.html': 'formset/bootstrap/widgets/radio.html',
+        'django/forms/widgets/radio.html': 'formset/bootstrap/widgets/multiple_input.html',
         'formset/default/widgets/file.html': 'formset/bootstrap/widgets/file.html',
-        'django/forms/widgets/checkbox.html': 'formset/default/widgets/input.html',
-        'django/forms/widgets/checkbox_select.html': 'formset/bootstrap/widgets/checkbox_select.html',
+        #'django/forms/widgets/checkbox.html': 'formset/default/widgets/input.html',
+        'django/forms/widgets/checkbox_select.html': 'formset/bootstrap/widgets/multiple_input.html',
     })
 
     def _amend_default_widget(self, context):
@@ -43,6 +43,7 @@ class FormRenderer(DefaultFormRenderer):
         for _, optgroup, _ in context['widget']['optgroups']:
             for option in optgroup:
                 option['attrs']['class'] = 'form-check-input'
+                option['template_name'] = 'formset/bootstrap/widgets/input_option.html'
         return context
 
     _context_modifiers = {
@@ -52,8 +53,10 @@ class FormRenderer(DefaultFormRenderer):
         'django/forms/widgets/file.html': _amend_default_widget,
         'django/forms/widgets/date.html': _amend_default_widget,
         'django/forms/widgets/select.html': _amend_select,
+        'formset/default/widgets/selectize.html': _amend_select,
         'django/forms/widgets/number.html': _amend_default_widget,
         'django/forms/widgets/password.html': _amend_default_widget,
+        'django/forms/widgets/textarea.html': _amend_default_widget,
         'django/forms/widgets/checkbox.html': _amend_checkbox,
         'django/forms/widgets/checkbox_select.html': _amend_multiple_input,
         'formset/default/widgets/file.html': _amend_file,
