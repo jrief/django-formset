f15712112554f20571d1f639cf903a9ca9d34ef3
Jacob Rief <jacob.rief@uibk.ac.at>
2022-04-28 10:09:16 +0200

    Refactor style loading into module

--- client/django-formset.ts
+++ client/django-formset.ts
@@ -1,13 +1,9 @@
-import styles from 'sass:./django-formset/DjangoFormset.scss';
 import { DjangoFormsetElement } from "./django-formset/DjangoFormset";
 import { DjangoSelectizeElement } from "./django-formset/DjangoSelectize";
 import { DualSelectorElement } from "./django-formset/DualSelector";
 
-const style = document.createElement('style');
-style.innerText = styles;
-document.head.appendChild(style)
-window.customElements.define('django-formset', DjangoFormsetElement);
 window.addEventListener('load', (event) => {
+	window.customElements.define('django-formset', DjangoFormsetElement);
 	window.customElements.define('django-selectize', DjangoSelectizeElement, {extends: 'select'});
 	window.customElements.define('django-dual-selector', DualSelectorElement, {extends: 'select'})
 });
