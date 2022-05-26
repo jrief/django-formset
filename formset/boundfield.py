8f125f289fa991e63c862bebfb355fb111eadbac
Jacob Rief <jacob.rief@gmail.com>
2022-05-26 13:36:38 +0200

    for field_css_classes, fall back to form name

--- formset/boundfield.py
+++ formset/boundfield.py
@@ -70,8 +70,7 @@ class BoundField(boundfield.BoundField):
         field_css_classes = getattr(self.form.renderer, 'field_css_classes', None)
         if isinstance(field_css_classes, dict):
             try:
-                prefix = f'{self.form.prefix}.{self.name}' if self.form.prefix else self.name
-                field_css_classes = field_css_classes[prefix]
+                field_css_classes = field_css_classes[self.name]
             except KeyError:
                 field_css_classes = field_css_classes.get('*')
         if hasattr(field_css_classes, 'split'):
