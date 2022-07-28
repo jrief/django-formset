jrief/django-formset

commit be91b71abb10a7986e7adfb2802155791df1de8e

create a deep copy of declared holders

formset/views.py
@@ -1,3 +1,4 @@
+import copy
 import json
 
 from django.http.response import HttpResponseBadRequest, JsonResponse
@@ -128,7 +129,7 @@ class FormView(IncompleSelectResponseMixin, FileUploadMixin, FormViewMixin, Gene
 class FormCollectionViewMixin(FormsetResponseMixin):
     collection_class = None
     success_url = None
-    initial = {}
+    initial = None
 
     def get(self, request, *args, **kwargs):
         """Handle GET requests: instantiate blank versions of the forms in the collection."""
@@ -164,7 +165,7 @@ class FormCollectionViewMixin(FormsetResponseMixin):
 
     def get_initial(self):
         """Return the initial data to use for collections of forms on this view."""
-        return self.initial.copy()
+        return copy.copy(self.initial)
 
     def get_success_url(self):
         return str(self.success_url) if self.success_url else None
@@ -191,7 +192,7 @@ class EditCollectionView(IncompleSelectResponseMixin, FileUploadMixin, FormColle
 
     def get_initial(self):
         initial = super().get_initial()
-        if self.object:
+        if isinstance(initial, dict) and self.object:
             collection_class = self.get_collection_class()
             initial.update(collection_class().model_to_dict(self.object))
         return initial

formset/utils.py
@@ -37,6 +37,10 @@ class HolderMixin:
 
     def replicate(self, data=None, initial=None, prefix=None, renderer=None, ignore_marked_for_removal=None):
         replica = copy.copy(self)
+        if hasattr(self, 'declared_holders'):
+            replica.declared_holders = {
+                key: holder.replicate() for key, holder in self.declared_holders.items()
+            }
         replica.data = data
         replica.is_bound = data is not None
         replica._errors = None

testapp/forms/nested.py
@@ -1,0 +1,41 @@
+from django.forms.fields import CharField
+from django.forms.forms import Form
+from formset.collection import FormCollection
+
+
+class CampumForm(Form):
+    agro = CharField(
+        label="Agro",
+        min_length=2,
+        max_length=12,
+    )
+
+
+class NestedCollection3(FormCollection):
+    min_siblings = 1
+    max_siblings = 4
+    extra_siblings = 2
+
+    campum = CampumForm()
+
+
+class NestedCollection2(FormCollection):
+    campum = CampumForm()
+
+    level3 = NestedCollection3()
+
+
+class NestedCollection1(FormCollection):
+    campum = CampumForm()
+
+    level2 = NestedCollection2()
+
+
+class NestedCollection(FormCollection):
+    min_siblings = 1
+    max_siblings = 5
+    extra_siblings = 1
+
+    campum = CampumForm()
+
+    level1 = NestedCollection1()

testapp/test_e2e_nested.py
@@ -1,0 +1,183 @@
+import json
+import pytest
+
+from django.urls import path
+
+from formset.utils import MARKED_FOR_REMOVAL
+from formset.views import FormCollectionView
+
+from .forms.nested import NestedCollection
+
+
+nested_collection_level1 = NestedCollection.declared_holders['level1'].replicate()
+nested_collection_level1.declared_holders['level2'].declared_holders['level3'].extra_siblings = 0
+
+
+class NestedCollectionNoSiblings(NestedCollection):
+    extra_siblings = 0
+
+    level1 = nested_collection_level1
+
+
+urlpatterns = [
+    path('nested', FormCollectionView.as_view(
+        collection_class=NestedCollection,
+        template_name='testapp/form-collection.html',
+        success_url='/success',
+    ), name='nested'),
+    path('nested_i', FormCollectionView.as_view(
+        collection_class=NestedCollectionNoSiblings,
+        template_name='testapp/form-collection.html',
+        success_url='/success',
+        initial=[{
+            'campum': {'agro': 'aa'},
+            'level1': {
+                'campum': {'agro': 'bb'},
+                'campum': {'agro': 'cc'},
+                'level2': {
+                    'level3': [
+                        {'campum': {'agro': 'dd'}},
+                        {'campum': {'agro': 'ee'}},
+                    ],
+                },
+            },
+        }],
+    ), name='nested_i'),
+]
+
+
+@pytest.mark.urls(__name__)
+@pytest.mark.parametrize('viewname', ['nested', 'nested_i'])
+def test_nested_default(page, mocker):
+    formset = page.query_selector('django-formset')
+    assert len(formset.query_selector_all('django-form-collection')) == 7
+    page.fill('#id_0\\.campum\\.agro', "AA")
+    page.fill('#id_0\\.level1\\.campum\\.agro', "BB")
+    page.fill('#id_0\\.level1\\.level2\\.campum\\.agro', "CC")
+    page.fill('#id_0\\.level1\\.level2\\.level3\\.0\\.campum\\.agro', "DD")
+    page.fill('#id_0\\.level1\\.level2\\.level3\\.1\\.campum\\.agro', "EE")
+    page.query_selector('#id_0\\.campum\\.agro').evaluate('elem => elem.focus()')
+    spy = mocker.spy(FormCollectionView, 'post')
+    page.wait_for_selector('django-formset').evaluate('elem => elem.submit()')
+    response = json.loads(spy.call_args.args[1].body)
+    assert response == {
+        'formset_data': [{
+            'level1': {
+                'level2': {
+                    'level3': [
+                        {'campum': {'agro': 'DD'}},
+                        {'campum': {'agro': 'EE'}},
+                    ],
+                    'campum': {'agro': 'CC'},
+                },
+                'campum': {'agro': 'BB'},
+            },
+            'campum': {'agro': 'AA'},
+        }]
+    }
+
+
+@pytest.mark.urls(__name__)
+@pytest.mark.parametrize('viewname', ['nested', 'nested_i'])
+def test_nested_add_inner(page, mocker):
+    formset = page.query_selector('django-formset')
+    assert len(formset.query_selector_all('django-form-collection')) == 7
+    page.fill('#id_0\\.campum\\.agro', "AA")
+    page.fill('#id_0\\.level1\\.campum\\.agro', "BB")
+    page.fill('#id_0\\.level1\\.level2\\.campum\\.agro', "CC")
+    page.fill('#id_0\\.level1\\.level2\\.level3\\.0\\.campum\\.agro', "DD")
+    page.fill('#id_0\\.level1\\.level2\\.level3\\.1\\.campum\\.agro', "EE")
+    page.click('template[prefix="0.level1.level2.level3"] + button.add-collection')
+    page.fill('#id_0\\.level1\\.level2\\.level3\\.2\\.campum\\.agro', "FF")
+    page.query_selector('#id_0\\.campum\\.agro').evaluate('elem => elem.focus()')
+    spy = mocker.spy(FormCollectionView, 'post')
+    page.wait_for_selector('django-formset').evaluate('elem => elem.submit()')
+    response = json.loads(spy.call_args.args[1].body)
+    assert response == {
+        'formset_data': [{
+            'level1': {
+                'level2': {
+                    'level3': [
+                        {'campum': {'agro': 'DD'}},
+                        {'campum': {'agro': 'EE'}},
+                        {'campum': {'agro': 'FF'}},
+                    ],
+                    'campum': {'agro': 'CC'},
+                },
+                'campum': {'agro': 'BB'},
+            },
+            'campum': {'agro': 'AA'},
+        }]
+    }
+
+
+@pytest.mark.urls(__name__)
+@pytest.mark.parametrize('viewname', ['nested', 'nested_i'])
+def test_nested_remove_inner_first(page, mocker, viewname):
+    formset = page.query_selector('django-formset')
+    assert len(formset.query_selector_all('django-form-collection')) == 7
+    page.fill('#id_0\\.campum\\.agro', "AA")
+    page.fill('#id_0\\.level1\\.campum\\.agro', "BB")
+    page.fill('#id_0\\.level1\\.level2\\.campum\\.agro', "CC")
+    page.click('template[prefix="0.level1.level2.level3"] + button.add-collection')
+    page.hover('#id_0\\.level1\\.level2\\.level3\\.0\\.campum')
+    formset.wait_for_selector('#id_0\\.level1\\.level2\\.level3\\.0\\.campum + button.remove-collection').click()
+    page.fill('#id_0\\.level1\\.level2\\.level3\\.1\\.campum\\.agro', "DD")
+    page.fill('#id_0\\.level1\\.level2\\.level3\\.2\\.campum\\.agro', "EE")
+    page.query_selector('#id_0\\.campum\\.agro').evaluate('elem => elem.focus()')
+    spy = mocker.spy(FormCollectionView, 'post')
+    page.wait_for_selector('django-formset').evaluate('elem => elem.submit()')
+    response = json.loads(spy.call_args.args[1].body)
+    assert response == {
+        'formset_data': [{
+            'level1': {
+                'level2': {
+                    'level3': [
+                        {'campum': {
+                            'agro': '' if viewname == 'nested' else 'dd',
+                            MARKED_FOR_REMOVAL: MARKED_FOR_REMOVAL
+                        }},
+                        {'campum': {'agro': 'DD'}},
+                        {'campum': {'agro': 'EE'}},
+                    ],
+                    'campum': {'agro': 'CC'},
+                },
+                'campum': {'agro': 'BB'},
+            },
+            'campum': {'agro': 'AA'},
+        }]
+    }
+
+
+@pytest.mark.urls(__name__)
+@pytest.mark.parametrize('viewname', ['nested', 'nested_i'])
+def test_nested_remove_inner_last(page, mocker):
+    formset = page.query_selector('django-formset')
+    assert len(formset.query_selector_all('django-form-collection')) == 7
+    page.fill('#id_0\\.campum\\.agro', "AA")
+    page.fill('#id_0\\.level1\\.campum\\.agro', "BB")
+    page.fill('#id_0\\.level1\\.level2\\.campum\\.agro', "CC")
+    page.click('template[prefix="0.level1.level2.level3"] + button.add-collection')
+    page.hover('#id_0\\.level1\\.level2\\.level3\\.2\\.campum')
+    formset.wait_for_selector('#id_0\\.level1\\.level2\\.level3\\.2\\.campum + button.remove-collection').click()
+    page.fill('#id_0\\.level1\\.level2\\.level3\\.0\\.campum\\.agro', "DD")
+    page.fill('#id_0\\.level1\\.level2\\.level3\\.1\\.campum\\.agro', "EE")
+    page.query_selector('#id_0\\.campum\\.agro').evaluate('elem => elem.focus()')
+    spy = mocker.spy(FormCollectionView, 'post')
+    page.wait_for_selector('django-formset').evaluate('elem => elem.submit()')
+    response = json.loads(spy.call_args.args[1].body)
+    assert response == {
+        'formset_data': [{
+            'level1': {
+                'level2': {
+                    'level3': [
+                        {'campum': {'agro': 'DD'}},
+                        {'campum': {'agro': 'EE'}},
+                    ],
+                    'campum': {'agro': 'CC'},
+                },
+                'campum': {'agro': 'BB'},
+            },
+            'campum': {'agro': 'AA'},
+        }]
+    }

commit 0591ae6b4eb1d47e511dfd08a81f2b46ac390480

fix regression introduced in be91b71abb10a7986e7adfb2802155791df1de8e

formset/views.py
@@ -1,4 +1,3 @@
-import copy
 import json
 
 from django.http.response import HttpResponseBadRequest, JsonResponse
@@ -129,7 +128,7 @@ class FormView(IncompleSelectResponseMixin, FileUploadMixin, FormViewMixin, Gene
 class FormCollectionViewMixin(FormsetResponseMixin):
     collection_class = None
     success_url = None
-    initial = None
+    initial = {}
 
     def get(self, request, *args, **kwargs):
         """Handle GET requests: instantiate blank versions of the forms in the collection."""
@@ -165,7 +164,7 @@ class FormCollectionViewMixin(FormsetResponseMixin):
 
     def get_initial(self):
         """Return the initial data to use for collections of forms on this view."""
-        return copy.copy(self.initial)
+        return self.initial.copy()
 
     def get_success_url(self):
         return str(self.success_url) if self.success_url else None