c7828e7a0948f2ac677d37cc816a3decb508539c
Jacob Rief <jacob.rief@gmail.com>
2022-07-30 18:19:11 +0200

    always create workdir and set permissions

--- .deployment/entrypoint.sh
+++ .deployment/entrypoint.sh
@@ -3,12 +3,8 @@ set -e
 echo "=== Starting django-formset testapp ==="
 
 [[ -z "$DJANGO_WORKDIR" ]] && echo >&2 "Missing environment variable: DJANGO_WORKDIR" && exit 1
-
-if [[ ! -d "$DJANGO_WORKDIR/media" ]] || [[ ! $(ls -A "$DJANGO_WORKDIR/media") ]]; then
-	# the directory where Django keeps its media is empty, so set the appropriate permissions
-	mkdir --parents "$DJANGO_WORKDIR"
-	chown -R django.django "$DJANGO_WORKDIR"
-fi
+mkdir --parents "$DJANGO_WORKDIR"
+chown -R django.django "$DJANGO_WORKDIR"
 
 export PYTHONPATH="/web"
 
