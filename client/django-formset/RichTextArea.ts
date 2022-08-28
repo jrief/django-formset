a48a5083e292f2d6d5727d0ae918cf68c3ffed01
Jacob Rief <jacob.rief@gmail.com>
2022-08-28 23:42:41 +0200

    WiP on RichTextEditor

--- client/django-formset/RichTextArea.ts
+++ client/django-formset/RichTextArea.ts
@@ -1,8 +1,12 @@
 import { Editor } from '@tiptap/core';
-import StarterKit from '@tiptap/starter-kit';
-import Document from '@tiptap/extension-document'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
+import Document from '@tiptap/extension-document';
+import Paragraph from '@tiptap/extension-paragraph';
+import Text from '@tiptap/extension-text';
+import Bold from '@tiptap/extension-bold';
+import Italic from '@tiptap/extension-italic';
+import Underline from '@tiptap/extension-underline';
+import BulletList from '@tiptap/extension-bullet-list';
+import ListItem from '@tiptap/extension-list-item';
 import styles from 'sass:./RichTextArea.scss';
 import { StyleHelpers } from "./helpers";
 
@@ -12,6 +16,7 @@ export class RichTextArea {
 	private readonly field: FieldGroup;
 	private readonly editor: Editor;
 	private readonly required: Boolean;
+	private readonly initialValue: string | object;
 
 	constructor(fieldGroup: FieldGroup, textAreaElement: HTMLTextAreaElement) {
 		this.declaredStyles = document.createElement('style');
@@ -20,20 +25,28 @@ export class RichTextArea {
 		this.field = fieldGroup;
 		this.editor = this.createEditor(textAreaElement);
 		this.required = textAreaElement.required;
-		//textAreaElement.required = false;
+		textAreaElement.required = false;
+		this.initialValue = textAreaElement.value;
 		textAreaElement.hidden = true;
 	}
 
 	private createEditor(textAreaElement: HTMLTextAreaElement) : Editor {
-		const divElement = document.createElement('DIV');
+		const divElement = document.createElement('DIV') as HTMLDivElement;
+		divElement.classList.add('richtext-wrapper');
 		textAreaElement.insertAdjacentElement('afterend', divElement);
 		const editor = new Editor({
 			element: divElement,
 			extensions: [
-				StarterKit,
 				Document,
 				Paragraph,
 				Text,
+				Bold,
+				Italic,
+				Underline,
+				BulletList,
+				ListItem.extend({
+					content: 'text*',
+				}),
 			],
 			content: '',
 			autofocus: false,
@@ -41,7 +54,49 @@ export class RichTextArea {
 			injectCSS: false,
 		});
 		this.transferStyles(textAreaElement);
+		this.wrapControlElements(divElement);
 		return editor;
+	}
+
+	private wrapControlElements(wrapperElement: HTMLElement) {
+		const buttonGroup = wrapperElement.parentElement?.querySelector('textarea[is="richtext"] ~ [role="group"]');
+		if (!buttonGroup)
+			throw new Error('<textarea is="richtext"> requires a sibling element <ANY [role="group"]>');
+		wrapperElement.insertBefore(buttonGroup, null);
+		buttonGroup.querySelectorAll('button[aria-label]').forEach(button => {
+			button.addEventListener('click', this.controlButtonClicked);
+		});
+	}
+
+	private controlButtonClicked = (event: Event) => {
+		if (event.currentTarget instanceof HTMLButtonElement && typeof event.currentTarget.ariaLabel === 'string') {
+			const parts = event.currentTarget.ariaLabel.split('-');
+			const funcname = parts.map((p, k) => k > 0 ? p.charAt(0).toUpperCase() + p.slice(1) : p).join('');
+			const func = this[funcname as keyof RichTextArea] as (() => void);
+			if (typeof func !== 'function')
+				throw new Error(`Unknown editor function '${funcname}'.`);
+			func.apply(this);
+		}
+	}
+
+	private bold() {
+		this.editor.chain().focus().toggleBold().run();
+	}
+
+	private italic() {
+		this.editor.chain().focus().toggleItalic().run();
+	}
+
+	private underline() {
+		this.editor.chain().focus().toggleUnderline().run();
+	}
+
+	private bulletList() {
+		this.editor.chain().focus().toggleBulletList().run();
+	}
+
+	private clearFormat() {
+		this.editor.chain().focus().unsetBold().unsetUnderline().unsetItalic().run();
 	}
 
 	private transferStyles(textareaElement: HTMLTextAreaElement) {
