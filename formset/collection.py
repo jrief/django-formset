01cc6f4e1eddfec6f70aff24b6f10932d391da2d
Jacob Rief <jacob.rief@gmail.com>
2022-09-17 11:40:12 +0200

    fix: initialized FormCollection with max_siblings=None
    fix:
    An initialized FormCollection with siblings but max_siblings=None, raised a TypeError

--- formset/collection.py
+++ formset/collection.py
@@ -121,7 +121,9 @@ class BaseFormCollection(HolderMixin, RenderableMixin):
             if not isinstance(self.initial, list):
                 errmsg = "{class_name} is declared to have siblings, but provided argument `{argument}` is not a list"
                 raise FormCollectionError(errmsg.format(class_name=self.__class__.__name__, argument='initial'))
-            num_siblings = min(self.max_siblings, max(num_siblings, len(self.initial)) + self.extra_siblings)
+            num_siblings = max(num_siblings, len(self.initial)) + self.extra_siblings
+            if self.max_siblings is not None:
+                num_siblings = min(self.max_siblings, num_siblings)
 
         first, last = 0, len(self.declared_holders.items()) - 1
         # add initialized collections/forms
