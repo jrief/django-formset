5dae62e1044420b11651ddf27d888bdf7d97e3fa
Jacob Rief <jacob.rief@gmail.com>
2022-10-20 17:54:00 +0200

    Fix some exception strings for Slug field

--- client/django-formset/DjangoSlug.ts
+++ client/django-formset/DjangoSlug.ts
@@ -5,10 +5,10 @@ export class DjangoSlugElement extends HTMLInputElement {
 	private connectedCallback() {
 		const populateFrom = this.getAttribute('populate-from');
 		if (!populateFrom)
-			throw new Error(`Field <input name="${this.name}" is="django-slug"> requires an attribute populate-from="..."`);
+			throw new Error(`Element ${this} requires an attribute populate-from="...".`);
 		const observedElement = this.form?.elements.namedItem(populateFrom);
 		if (!(observedElement instanceof HTMLInputElement))
-			throw new Error(`Field <input name="${populateFrom}"> is missing on this form.`);
+			throw new Error(`Element <input name="${populateFrom}"> is missing on this form.`);
 		if (this.value === '') {
 			observedElement.addEventListener('input', (event: Event) => {
 				if (event.currentTarget instanceof HTMLInputElement) {
