5009e5e647788da4f72d51da4566f0a7544fb06a
Jacob Rief <jacob.rief@gmail.com>
2022-10-21 10:56:47 +0200

    Revert "Next attempt for dynamic loading with newer esbuild"
    This reverts commit 98a3816fac22e1b63cc0e1c84748defb8d1331fe.

--- client/django-formset.ts
+++ client/django-formset.ts
@@ -1,46 +1,18 @@
 import { DjangoFormsetElement } from "./django-formset/DjangoFormset";
 import { StyleHelpers } from './django-formset/helpers';
+import { DjangoSelectizeElement } from "./django-formset/DjangoSelectize";
+import { DualSelectorElement } from "./django-formset/DualSelector";
+import { SortableSelectElement } from "./django-formset/SortableSelect";
+import { RichTextAreaElement } from "./django-formset/RichtextArea";
+import { DjangoSlugElement } from "./django-formset/DjangoSlug";
 
 window.addEventListener('load', (event) => {
 	const pseudoStylesElement = StyleHelpers.convertPseudoClasses();
-	const promises = Array<Promise<any>>();
-	if (document.querySelector('select[is="django-selectize"]')) {
-		const promise = import('./django-formset/DjangoSelectize');
-		promise.then(({DjangoSelectizeElement}) => {
-			window.customElements.define('django-selectize', DjangoSelectizeElement, {extends: 'select'});
-		});
-		promises.push(promise);
-	}
-	if (document.querySelector('django-sortable-select')) {
-		const promise = import('./django-formset/SortableSelect');
-		promise.then(({ SortableSelectElement }) => {
-			window.customElements.define('django-sortable-select', SortableSelectElement);
-		});
-		promises.push(promise);
-	}
-	if (document.querySelector('select[is="django-dual-selector"]')) {
-		const promise = import('./django-formset/DualSelector');
-		promise.then(({ DualSelectorElement }) => {
-			window.customElements.define('django-dual-selector', DualSelectorElement, {extends: 'select'});
-		});
-		promises.push(promise);
-	}
-	if (document.querySelector('textarea[is="django-richtext"]')) {
-		const promise = import('./django-formset/RichtextArea');
-		promise.then(({ RichTextAreaElement }) => {
-	 		window.customElements.define('django-richtext', RichTextAreaElement, {extends: 'textarea'});
-		});
-		promises.push(promise);
-	}
-	if (document.querySelector('input[is="django-slug"]')) {
-		const promise = import('./django-formset/DjangoSlug');
-		promise.then(({ DjangoSlugElement }) => {
-	 		window.customElements.define('django-richtext', DjangoSlugElement, {extends: 'input'});
-		});
-		promises.push(promise);
-	}
-	Promise.all(promises).then(() => {
-		window.customElements.define('django-formset', DjangoFormsetElement);
-		pseudoStylesElement.remove();
-	});
+	window.customElements.define('django-selectize', DjangoSelectizeElement, {extends: 'select'});
+	window.customElements.define('django-sortable-select', SortableSelectElement);
+	window.customElements.define('django-dual-selector', DualSelectorElement, {extends: 'select'});
+	window.customElements.define('django-richtext', RichTextAreaElement, {extends: 'textarea'});
+	window.customElements.define('django-slug', DjangoSlugElement, {extends: 'input'});
+	window.customElements.define('django-formset', DjangoFormsetElement);
+	pseudoStylesElement.remove();
 });
