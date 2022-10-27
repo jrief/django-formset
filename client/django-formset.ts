6023e11c8f5d5ba2ecee0fc8b3353f92c9bfbeda
Jacob Rief <jacob.rief@gmail.com>
2022-10-27 22:44:49 +0200

    Add linters: flake8 and isort

--- client/django-formset.ts
+++ client/django-formset.ts
@@ -4,12 +4,12 @@ import { StyleHelpers } from './django-formset/helpers';
 
 window.addEventListener('DOMContentLoaded', (event) => {
 	const pseudoStylesElement = StyleHelpers.convertPseudoClasses();
-	const promises = Array<Promise<string>>();
+	const promises = Array<Promise<void>>();
 	if (document.querySelector('select[is="django-selectize"]')) {
 		promises.push(new Promise((resolve, reject) => {
 			import('./django-formset/DjangoSelectize').then(({DjangoSelectizeElement}) => {
 				window.customElements.define('django-selectize', DjangoSelectizeElement, {extends: 'select'});
-				window.customElements.whenDefined('django-selectize').then(() => resolve('django-selectize'));
+				window.customElements.whenDefined('django-selectize').then(() => resolve());
 			}).catch(err => reject(err));
 		}));
 	}
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 		promises.push(new Promise((resolve, reject) => {
 			import('./django-formset/SortableSelect').then(({ SortableSelectElement }) => {
 				window.customElements.define('django-sortable-select', SortableSelectElement);
-				window.customElements.whenDefined('django-sortable-select').then(() => resolve('django-sortable-select'));
+				window.customElements.whenDefined('django-sortable-select').then(() => resolve());
 			}).catch(err => reject(err));
 		}));
 	}
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 		promises.push(new Promise((resolve, reject) => {
 			import('./django-formset/DualSelector').then(({ DualSelectorElement }) => {
 				window.customElements.define('django-dual-selector', DualSelectorElement, {extends: 'select'});
-				window.customElements.whenDefined('django-dual-selector').then(() => resolve('django-dual-selector'));
+				window.customElements.whenDefined('django-dual-selector').then(() => resolve());
 			}).catch(err => reject(err));
 		}));
 	}
@@ -33,7 +33,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 		promises.push(new Promise((resolve, reject) => {
 			import('./django-formset/RichtextArea').then(({ RichTextAreaElement }) => {
 				window.customElements.define('django-richtext', RichTextAreaElement, {extends: 'textarea'});
-				window.customElements.whenDefined('django-richtext').then(() => resolve('django-richtext'));
+				window.customElements.whenDefined('django-richtext').then(() => resolve());
 			}).catch(err => reject(err));
 		}));
 	}
@@ -41,12 +41,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
 		promises.push(new Promise((resolve, reject) => {
 			import('./django-formset/DjangoSlug').then(({ DjangoSlugElement }) => {
 		 		window.customElements.define('django-slug', DjangoSlugElement, {extends: 'input'});
-				window.customElements.whenDefined('django-slug').then(() => resolve('django-slug'));
+				window.customElements.whenDefined('django-slug').then(() => resolve());
 			}).catch(err => reject(err));
 		}));
 	}
-	Promise.all(promises).then((values: Array<string>) => {
-		// console.log(values);
+	Promise.all(promises).then(() => {
 		window.customElements.define('django-formset', DjangoFormsetElement);
 		pseudoStylesElement.remove();
 	}).catch(error => console.error(`Failed to initialize django-formset: ${error}`));
