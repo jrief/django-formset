97fe2a97fa642a95169f3e0ac586bcb9f3457c25
Jacob Rief <jacob.rief@gmail.com>
2023-01-26 23:16:53 +0100

    add form for testing

--- testapp/forms/birthdate.py
+++ testapp/forms/birthdate.py
@@ -3,15 +3,22 @@ from datetime import timedelta
 from django.forms import fields, forms
 from django.utils.timezone import datetime, now
 
-from formset.widgets import DatePicker, DateInput
+from formset.widgets import DatePicker, DateTimePicker
 
 
 class BirthdateForm(forms.Form):
     birthdate = fields.DateField(
         label="Birthdate",
-        widget=DatePicker(attrs={
-            'min': lambda: (now().date() + timedelta(weeks=1)).isoformat(),
-            'max': lambda: (now().date() + timedelta(weeks=6)).isoformat(),
+        widget=DatePicker(),
+#        initial=datetime(1981, 7, 9),
+    )
+
+    schedule = fields.DateTimeField(
+        label="Schedule",
+        widget=DateTimePicker(attrs={
+            'min': lambda: (now().date() - timedelta(days=1)).isoformat(),
+            'max': lambda: (now().date() + timedelta(weeks=30)).isoformat(),
+            'step': timedelta(minutes=5),
         }),
-        initial=datetime(2023, 3, 3),
+        initial=datetime(2023, 2, 2, 9, 10),
     )
