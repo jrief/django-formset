357b0deb3daef1a25bf42ffa137355359995403d
Jacob Rief <jacob.rief@gmail.com>
2023-04-11 20:57:17 +0200

    add feature align text in RichTextarea

--- formset/richtext/controls.py
+++ formset/richtext/controls.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ImproperlyConfigured
 from django.template.loader import select_template
 from django.utils.translation import gettext_lazy as _
 
@@ -40,6 +41,45 @@ class Heading(ControlElement):
     def render(self, renderer):
         template = self.get_template(renderer)
         return template.render({'levels': self.levels})
+
+
+class TextAlign(ControlElement):
+    name = 'heading'
+    label = _("Text Align")
+    alignments = ['left', 'center', 'right', 'justify']
+    template_name = 'formset/{framework}/buttons/richtext_align.html'
+
+    def __init__(self, alignments=None, default_alignment=None):
+        if isinstance(alignments, str):
+            alignments = [alignments]
+        elif not isinstance(alignments, (list, tuple)):
+            alignments = self.alignments
+        elif len(alignments) == 0:
+            raise ImproperlyConfigured(
+                f"At least one alignment must be given for {self.__class__.__name__}"
+            )
+        for a in alignments:
+            if a not in self.alignments:
+                raise ImproperlyConfigured(
+                    f"'{a}' in not a valid alignment for {self.__class__.__name__}"
+                )
+        self.alignments = alignments
+
+        if default_alignment is None:
+            self.default_alignment = self.alignments[0]
+        elif default_alignment not in self.alignments:
+            raise ImproperlyConfigured(
+                f"'{default_alignment}' in not a valid default alignment for {self.__class__.__name__}"
+            )
+        else:
+            self.default_alignment = default_alignment
+
+    def render(self, renderer):
+        template = self.get_template(renderer)
+        return template.render({
+            'alignments': self.alignments,
+            'default_alignment': self.default_alignment,
+        })
 
 
 class Bold(ControlElement):
