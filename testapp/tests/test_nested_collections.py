e399f5aa713ef1bacd33065a2f60f15a89f4ce7a
Jacob Rief <jacob.rief@gmail.com>
2023-06-12 23:27:49 +0200

    more testst for nested collection

--- testapp/tests/test_nested_collections.py
+++ testapp/tests/test_nested_collections.py
@@ -1,18 +1,15 @@
 import json
 
-import bs4
 import pytest
 from bs4 import BeautifulSoup, Tag
 
-from django.test import Client, RequestFactory
-from django.utils.timezone import datetime
-from django.views.generic.edit import CreateView, UpdateView
+from django.core.exceptions import NON_FIELD_ERRORS
+from django.test import RequestFactory
 
-from formset.views import EditCollectionView, BulkEditCollectionView
+from formset.views import EditCollectionView
 
 from testapp.forms.company import CompanyCollection
 from testapp.models.company import Company, Department, Team
-
 
 
 class CompanyCollectionView(EditCollectionView):
@@ -82,15 +79,21 @@ def test_create_company_with_department(single_collection_view):
 @pytest.fixture
 def created_company():
     company = Company.objects.create(name="Pepsi")
-    Department.objects.create(name="Finance", company=company)
+    department = Department.objects.create(name="Finance", company=company)
+    Team.objects.create(name="Accounting", department=department)
     return company
 
 @pytest.mark.django_db
-def test_edit_company_department(single_collection_view, created_company):
+def test_edit_copmany_and_department_and_team(single_collection_view, created_company):
     form_data = {
         'formset_data': {
             'departments': [{
-                "teams": [],
+                "teams": [{
+                    "team": {
+                        "name": "Social Media",
+                        "id": created_company.departments.first().teams.first().id,
+                    }
+                }],
                 "department": {
                     "name": "Marketing",
                     "id": created_company.departments.first().id,
@@ -106,10 +109,11 @@ def test_edit_company_department(single_collection_view, created_company):
     company = Company.objects.last()
     assert company.name == "Coke"
     assert company.departments.first().name == "Marketing"
+    assert company.departments.first().teams.first().name == "Social Media"
 
 
 @pytest.mark.django_db
-def test_edit_company_add_department(single_collection_view, created_company):
+def test_add_department(single_collection_view, created_company):
     department = created_company.departments.first()
     form_data = {
         'formset_data': {
@@ -139,7 +143,7 @@ def test_edit_company_add_department(single_collection_view, created_company):
 
 
 @pytest.mark.django_db
-def test_edit_company_ununique_department(single_collection_view, created_company):
+def test_check_unique_department(single_collection_view, created_company):
     department = created_company.departments.first()
     form_data = {
         'formset_data': {
@@ -160,9 +164,8 @@ def test_edit_company_ununique_department(single_collection_view, created_compan
     }
     request = RequestFactory().post('/', form_data, content_type='application/json')
     response = single_collection_view(request)
-    assert response.status_code == 200
-    assert json.loads(response.getvalue())['success_url'] == '/success'
-    company = Company.objects.last()
-    assert company.name == "Pepsi"
-    assert company.departments.order_by('id').first().name == "Finance"
-    assert company.departments.order_by('id').last().name == "Marketing"
+    assert response.status_code == 422
+    response_body = json.loads(response.getvalue())
+    assert 'success_url' not in response_body
+    expected = ['Please correct the duplicate data for name and company, which must be unique.']
+    assert response_body['departments'][1]['department'][NON_FIELD_ERRORS] == expected
