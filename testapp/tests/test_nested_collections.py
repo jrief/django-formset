31138bce0e968df58cc62e1e16675cd5960d2683
Jacob Rief <jacob.rief@gmail.com>
2023-07-26 15:12:38 +0200

    add extra tests for use cases with boguous data

--- testapp/tests/test_nested_collections.py
+++ testapp/tests/test_nested_collections.py
@@ -4,9 +4,11 @@ import pytest
 from bs4 import BeautifulSoup, Tag
 
 from django.core.exceptions import NON_FIELD_ERRORS
+from django.forms import fields, forms
 from django.test import RequestFactory
 
-from formset.views import EditCollectionView
+from formset.collection import COLLECTION_ERRORS, FormCollection
+from formset.views import EditCollectionView, FormCollectionView
 
 from testapp.forms.company import CompanyCollection
 from testapp.models.company import Company, Department, Team
@@ -169,3 +171,145 @@ def test_check_unique_department(single_collection_view, created_company):
     assert 'success_url' not in response_body
     expected = ['Please correct the duplicate data for name and company, which must be unique.']
     assert response_body['departments'][1]['department'][NON_FIELD_ERRORS] == expected
+
+
+class PersonForm(forms.Form):
+    full_name = fields.CharField(
+        label="Full name",
+        min_length=2,
+        max_length=50,
+    )
+
+
+class PhoneNumberForm(forms.Form):
+    phone_number = fields.RegexField(
+        r'^[01+][ 0-9.\-]+$',
+        label="Phone Number",
+        min_length=2,
+        max_length=20,
+    )
+
+
+class PhoneNumberCollection(FormCollection):
+    min_siblings = 1
+    max_siblings = 3
+    extra_siblings = 1
+    number = PhoneNumberForm()
+
+
+class ContactCollection(FormCollection):
+    person = PersonForm()
+    numbers = PhoneNumberCollection()
+
+
+@pytest.fixture
+def contact_collection_view():
+    return FormCollectionView.as_view(
+        collection_class=ContactCollection,
+    )
+
+
+def test_check_too_many_collections(contact_collection_view):
+    form_data = {
+        'formset_data': {
+            'person': {
+                'full_name': "John Doe",
+            },
+            'numbers': [
+                {'number': {'phone_number': "+1 234 567 8900"}},
+                {'number': {'phone_number': "+33 1 43478293"}},
+                {'number': {'phone_number': "+39 335 327041"}},
+                {'number': {'phone_number': "+41 91 667914"}},
+            ],
+        }
+    }
+    request = RequestFactory().post('/', form_data, content_type='application/json')
+    response = contact_collection_view(request)
+    assert response.status_code == 422
+    response_body = json.loads(response.getvalue())
+    assert response_body == {
+        'person': {},
+        'numbers': [{COLLECTION_ERRORS: ['Too many entries in “PhoneNumberCollection”, please remove one.']}]
+    }
+
+
+def test_check_too_few_collections(contact_collection_view):
+    form_data = {
+        'formset_data': {
+            'person': {
+                'full_name': "John Doe",
+            },
+            'numbers': [],
+        }
+    }
+    request = RequestFactory().post('/', form_data, content_type='application/json')
+    response = contact_collection_view(request)
+    assert response.status_code == 422
+    response_body = json.loads(response.getvalue())
+    assert response_body == {
+        'person': {},
+        'numbers': [{COLLECTION_ERRORS: ['Not enough entries in “PhoneNumberCollection”, please add another.']}]
+    }
+
+
+def test_missing_formset_data(contact_collection_view):
+    form_data = {
+        'person': {
+            'full_name': "John Doe",
+        },
+        'numbers': [],
+    }
+    request = RequestFactory().post('/', form_data, content_type='application/json')
+    response = contact_collection_view(request)
+    assert response.status_code == 422
+    response_body = json.loads(response.getvalue())
+    assert response_body == {
+        'person': {NON_FIELD_ERRORS: ['Form data is missing.']},
+        'numbers': {NON_FIELD_ERRORS: ['Form data is missing.']}
+    }
+
+
+def test_check_bogous_formset_data(contact_collection_view):
+    form_data = {
+        'formset_data': [0, 'A', 2]
+    }
+    request = RequestFactory().post('/', form_data, content_type='application/json')
+    response = contact_collection_view(request)
+    assert response.status_code == 422
+    response_body = json.loads(response.getvalue())
+    assert response_body == {
+        'person': {NON_FIELD_ERRORS: ['Form data is missing.']},
+        'numbers': {NON_FIELD_ERRORS: ['Form data is missing.']}
+    }
+
+
+def test_check_boguous_collection_data(contact_collection_view):
+    form_data = {
+        'person': [0, 'A', 2],
+        'numbers': {
+            'full_name': "John Doe"
+        }
+    }
+    request = RequestFactory().post('/', form_data, content_type='application/json')
+    response = contact_collection_view(request)
+    assert response.status_code == 422
+    response_body = json.loads(response.getvalue())
+    assert response_body == {
+        'person': {NON_FIELD_ERRORS: ['Form data is missing.']},
+        'numbers': {NON_FIELD_ERRORS: ['Form data is missing.']}
+    }
+
+
+def test_check_missing_collection_data(contact_collection_view):
+    form_data = {
+        'person': None,
+        'numbers': None,
+    }
+    request = RequestFactory().post('/', form_data, content_type='application/json')
+    response = contact_collection_view(request)
+    assert response.status_code == 422
+    response_body = json.loads(response.getvalue())
+    assert response_body == {
+        'person': {NON_FIELD_ERRORS: ['Form data is missing.']},
+        'numbers': {NON_FIELD_ERRORS: ['Form data is missing.']}
+    }
