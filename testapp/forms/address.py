5318b0524b9c6b65b8057cfc2fcc08c1d124c1d5
Jacob Rief <jacob.rief@gmail.com>
2023-11-21 22:59:59 +0100

    remove verbose docstrings in demo view classes

--- testapp/forms/address.py
+++ testapp/forms/address.py
@@ -4,33 +4,6 @@ from django.forms import fields, forms
 class AddressForm(forms.Form):
     """
     Grouping fields using CSS
-    -------------------------
-
-    Sometimes it is desirable to align certain input fields in the same row. To achieve this, the Bootstrap
-    framework offers special CSS classes, in order to style the input groups into columns of different size.
-
-    We can use this feature, by adding a dictionary to our Form class:
-
-    .. code-block:: python
-
-        from formset.renderers.bootstrap import FormRenderer
-
-        class AddressForm(...):
-            ...
-            default_renderer = FormRenderer(
-                form_css_classes='row',
-                field_css_classes={
-                    'postal_code': 'mb-2 col-4',
-                    'city': 'mb-2 col-8',
-                    '*': 'mb-2 col-12',
-                },
-            })
-            ...
-
-    This overwrites the renderer for this Form class. Here the CSS classes for the named fields ``postal_code`` and
-    ``city`` replace the given defaults in the HTML element wrapping the input field(s),
-    ie. ``<div role="group" class="...">``. The key value ``'*'`` is used as a wildcard matching all other field
-    names.
     """
 
     recipient = fields.CharField(
--- testapp/forms/contact.py
+++ testapp/forms/contact.py
@@ -25,34 +25,6 @@ class ProfessionForm(forms.Form):
 class SimpleContactCollection(FormCollection):
     """
     A Form Collection can be used to combine two or more Django Forms into one collection.
-
-    A simple collection may look like:
-
-    .. code-block:: python
-
-        from formset.collection import FormCollection
-
-        class PersonForm(forms.Form):
-            last_name = fields.CharField(…)
-            first_name = fields.CharField(…)
-
-        class ProfessionForm(forms.Form):
-            company = fields.CharField(…)
-            job_title = fields.CharField(…, required=False)
-
-        class SimpleContactCollection(FormCollection):
-            person = PersonForm()
-            profession = ProfessionForm()
-
-    Such a collection behaves similar to a single form in the sense, that those two child
-    forms, ``PersonForm`` and ``ProfessionForm``, are validated and submitted altogether.
-
-    The dictionary with the submitted form data then contains two sub-dictionaries named
-    ``person`` and ``profession``. Those sub-dictionaries then contain the form data for
-    each form respectively.
-
-    Such Collections can nest other Form Collections. This allows us to build hierarchies
-    of forms with many nesting levels.
     """
 
     person = PersonForm()
@@ -108,55 +80,7 @@ class PhoneNumberCollection(FormCollection):
 class ContactCollection(FormCollection):
     """
     This Form Collection shows how to combine two independend forms into one collection,
-    where the second form is wrapped into a *collection with siblings*. This allows us to
-    submit siblings of the same form definition multiple times.
-
-    Such a nested collection looks like:
-
-    .. code-block:: python
-
-        from formset.collection import FormCollection
-
-        class PhoneNumberForm(forms.Form):
-            phone_number = fields.RegexField(…)
-            label = fields.ChoiceField(…)
-
-        class PhoneNumberCollection(FormCollection):
-            min_siblings = 1
-            max_siblings = 5
-            extra_siblings = 1
-
-            number = PhoneNumberForm()
-
-    The ``PhoneNumberCollection`` is a collection of another form, ie. ``PhoneNumberForm``,
-    containg a phone number and a label field. That collection can contain up to five labeled
-    phone numbers. Here the attributes ``min_siblings`` and ``max_siblings`` specify the minimum
-    and maximum number of siblings for that collection. If ``max_siblings`` is unset or ``None``,
-    an infinite number of siblings can be added. The attrinute ``extra_siblings`` specifies how
-    many empty form collections shall be added.
-
-    Finally, we glue those collections together:
-
-    .. code-block:: python
-
-        from formset.collection import FormCollection
-
-        class PersonForm(forms.Form):
-            last_name = fields.CharField(…)
-            # … other fields
-
-        class ContactCollection(FormCollection):
-            legend = "Contact"
-            person = PersonForm()
-            numbers = PhoneNumberCollection()
-
-    This gives us a nice form interface where we can add up to five phone numbers to each
-    contact we have. Whenever we want to add a new sibling, we press onto the **+** symbol
-    below the phone number collection. We can also delete siblings be clicking on the trash
-    symbol, rendered while hovering over the corresponding phone number form.
-
-    The framework always keeps track on the minimum and maximum number of siblings and
-    wouldn't let us add or remove any sibling outside the allowed range.
+    where the second form is wrapped into a *collection with siblings*.
     """
     legend = "Contact"
     # ignore_marked_for_removal = True
@@ -170,28 +94,6 @@ class ContactCollectionList(FormCollection):
     """
     This Form Collection shows how to start right away as a *collection with siblings*. It can
     be used to create an editable list view of one or more forms.
-
-    This collection is an extension of the previous example, where the outhermost collection
-    is declared to allow siblings.
-
-    .. code-block:: python
-
-        from formset.collection import FormCollection
-
-        class ContactCollectionList(FormCollection):
-            legend = "List of Contacts"
-            min_siblings = 0
-            extra_siblings = 1
-
-            person = PersonForm()
-            numbers = PhoneNumberCollection(
-                min_siblings=0,
-                max_siblings=3,
-                extra_siblings=0,
-            )
-
-    By passing in other values to the collection's constructor, we can respecify the
-    number of mininimum, maximum and extra siblings.
     """
     legend = "List of Contacts"
     add_label = "Add new Contact"
@@ -211,33 +113,6 @@ class ContactCollectionList(FormCollection):
 class SortableContactCollection(FormCollection):
     """
     Sortable Collections
-    --------------------
-
-    This is a variation of the ``ContactCollection`` demo, where we reuse the class
-    ``PhoneNumberCollection``, and parametrize it be *sortable*. This allows the user to sort each
-    child collection containing the phone number and label fields. The sorting can be performed by
-    dragging the supplied drag handle in the top right corner of each sub collection.
-
-    Declaring a ``FormCollection`` as sortable, is as simple as either adding the attribute
-    ``is_sortable = True`` to the collection class or by instantiating the collection class
-    as sortable. In this example we do the latter reusing the ``PhoneNumberCollection`` previously
-    defined:
-
-    .. code-block:: python
-
-        from formset.collection import FormCollection
-        from myproject.forms import PersonForm, PhoneNumberCollection
-
-        class SortableContactCollection(FormCollection):
-            person = PersonForm(initial={…})
-
-            numbers = PhoneNumberCollection(
-                max_siblings=5,
-                is_sortable=True,
-                initial=[…],
-                help_text="A maximum of 5 phone numbers are allowed per contact.",
-            )
-
     """
 
     person = PersonForm(initial={
@@ -259,30 +134,6 @@ class SortableContactCollection(FormCollection):
 
 
 class SortableContactCollectionList(ContactCollectionList):
-    """
-    This is a variation of the ``ContactCollectionList`` demo, which is parametrized to be sortable
-    together with its sub form ``PhoneNumberCollection``. This allows the user to sort the outer
-    collections separately from its inner collections, by using the supplied drag handle on the top
-    right of each collection.
-
-    Declaring a ``FormCollection`` as sortable, is as simple as either adding the attribute
-    ``is_sortable = True`` to the collection class or by instantiating the collection class
-    as sortable. By inheriting from ``ContactCollectionList`` we can add the attribute
-    ``is_sortable`` to that class, while for its inner collections we reinstantiate it using
-    ``PhoneNumberCollection(is_sortable=True)``.
-
-    .. code-block:: python
-
-        from myproject.forms import (ContactCollectionList,
-                                     PhoneNumberCollection)
-
-        class SortableContactCollectionList(ContactCollectionList):
-            is_sortable = True
-
-            numbers = PhoneNumberCollection(is_sortable=True)
-
-    """
-
     is_sortable = True
 
     numbers = PhoneNumberCollection(is_sortable=True)
@@ -303,41 +154,6 @@ class IntermediateCollection(FormCollection):
 class IntermediateContactCollectionList(FormCollection):
     """
     Wrap a FormCollection into another FormCollection
-    -------------------------------------------------
-
-    Here we wrap an existing FormCollection with siblings, into another FormCollection.
-    Apart from adding an extra nesting level in the submitted data, this does not
-    have any side effects.
-
-    Using this might be useful if you want to add the legend and/or the help text outside the block
-    containing the sortable collection of phone numbers.
-
-    .. code-block:: python
-
-        from formset.collection import FormCollection
-        from myapp.forms import PersonForm
-
-        class IntermediateCollection(FormCollection):
-            help_text = "This intermediate collection adds another level of nesting without any benefit."
-
-            numbers = PhoneNumberCollection(
-                min_siblings=0,
-                max_siblings=3,
-                extra_siblings=0,
-                is_sortable=True,
-                help_text="Resort the phone numbers by using the drag handle on the upper right.",
-            )
-
-        class IntermediateContactCollectionList(FormCollection):
-            legend = "List of Contacts"
-            help_text = "Contacts can not be sorted, only their phone numbers."
-            add_label = "Add new Contact"
-            min_siblings = 0
-            extra_siblings = 1
-
-            person = PersonForm()
-
-            intermediate = IntermediateCollection()
     """
     legend = "List of Contacts"
     help_text = "Contacts can not be sorted, only their phone numbers."
