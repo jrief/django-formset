jrief/django-formset

commit af3b79873de67cb2de23beab7776f701e7555c0a

start working on a generic FormDialog

client/django-formset/DjangoFormset.ts
@@ -899,6 +899,150 @@ class DjangoFieldset {
 }
 
 
+class FormDialog {
+	private readonly form: DjangoForm;
+	private readonly element: HTMLDialogElement;
+	private readonly dialogHeaderElement: HTMLElement | null;
+	private readonly formElement: HTMLFormElement;
+	private readonly opener: Function;
+	private readonly baseSelector = 'dialog[is="django-dialog-form"]';
+	private dialogRect: DOMRect | null = null;
+	private dialogOffsetX: number = 0;
+	private dialogOffsetY: number = 0;
+
+	constructor(form: DjangoForm, element: HTMLDialogElement) {
+		this.form = form;
+		this.element = element;
+		this.formElement = this.element.querySelector('form[method="dialog"]')!;
+		if (!this.formElement)
+			throw new Error(`${this} requires child <form method="dialog">`);
+		this.opener = this.evalOpener();
+		this.dialogHeaderElement = this.element.querySelector('.dialog-header');
+		if (!StyleHelpers.stylesAreInstalled(this.baseSelector)) {
+			this.transferStyles();
+		}
+	}
+
+	private evalOpener(): Function {
+		const attrValue = this.element?.getAttribute('df-open-condition');
+		if (typeof attrValue !== 'string')
+			return () => {};
+		try {
+			const evalExpression = new Function(`return ${parse(attrValue, {startRule: 'Expression'})}`);
+			return () => {
+				if (evalExpression.call(this)) {
+					this.openDialog();
+				}
+			};
+		} catch (error) {
+			throw new Error(`Error while parsing <dialog df-open-condition="${attrValue}">: ${error}.`);
+		}
+	}
+
+	private handleCloseButton() {
+		const closeButton = this.formElement.elements.namedItem('close');
+		if (!(closeButton instanceof HTMLButtonElement))
+			return;
+		closeButton.addEventListener('click', () => {
+			this.element.close('close')
+		}, {once: true});
+	}
+
+	private handleSaveButton() {
+		const saveButton = this.formElement.elements.namedItem('save');
+		if (!(saveButton instanceof HTMLButtonElement))
+			return;
+		saveButton.addEventListener('click', () => {
+			if (this.formElement.checkValidity()) {
+				this.element.close('save');
+			}
+		});
+	}
+
+	private openDialog = () => {
+		if (this.element.open)
+			return;
+		this.handleCloseButton();
+		this.handleSaveButton();
+		this.element.show();
+		this.element.addEventListener('close', this.closeDialog, {once: true});
+		if (this.dialogHeaderElement) {
+			this.dialogOffsetX = this.dialogOffsetY = 0;
+			this.dialogRect = this.element.getBoundingClientRect();
+			this.dialogHeaderElement.addEventListener('pointerdown', this.handlePointerDown);
+			this.dialogHeaderElement.addEventListener('touchstart', this.handlePointerDown);
+		}
+	};
+
+	private closeDialog = () => {
+		this.element.close();
+	};
+
+	private handlePointerDown = (event: PointerEvent | TouchEvent) => {
+		const viewport = window.visualViewport;
+		let offsetX: number;
+		let offsetY: number;
+
+		const moveDialog = (pointerX: number, pointerY: number) => {
+			this.dialogOffsetX = Math.max(pointerX - offsetX, -this.dialogRect!.left);
+			this.dialogOffsetY = Math.max(pointerY - offsetY, -this.dialogRect!.top);
+			this.dialogOffsetX = Math.min(this.dialogOffsetX, viewport!.width - this.dialogRect!.right);
+			this.dialogOffsetY = Math.min(this.dialogOffsetY, viewport!.height - this.dialogRect!.bottom);
+			console.log(this.dialogOffsetX, this.dialogOffsetY);
+			this.element.style.transform = `translate(${this.dialogOffsetX}px, ${this.dialogOffsetY}px)`;
+		};
+		const handlePointerMove = (pointerMoveEvt: PointerEvent) => {
+			moveDialog(pointerMoveEvt.clientX, pointerMoveEvt.clientY);
+		};
+		const handleTouchMove = (touchMoveEvt: TouchEvent) => {
+			touchMoveEvt.preventDefault()
+			moveDialog(touchMoveEvt.touches[0].clientX, touchMoveEvt.touches[0].clientY);
+		};
+		const handlePointerUp = (pointerUpEvt: PointerEvent) => {
+			this.dialogHeaderElement!.releasePointerCapture(pointerUpEvt.pointerId);
+			this.dialogHeaderElement!.removeEventListener('pointermove', handlePointerMove);
+		};
+		const handleTouchEnd = (touchEndEvt: TouchEvent) => {
+			this.dialogHeaderElement!.removeEventListener('touchmove', handleTouchMove);
+		}
+
+		if (event instanceof PointerEvent) {
+			offsetX = event.clientX - this.dialogOffsetX;
+			offsetY = event.clientY - this.dialogOffsetY;
+			this.dialogHeaderElement!.setPointerCapture(event.pointerId);
+			this.dialogHeaderElement!.addEventListener('pointermove', handlePointerMove);
+			this.dialogHeaderElement!.addEventListener('pointerup', handlePointerUp, {once: true});
+		} else {
+			offsetX = event.touches[0].clientX - this.dialogOffsetX;
+			offsetY = event.touches[0].clientY - this.dialogOffsetY;
+			this.dialogHeaderElement!.addEventListener('touchmove', handleTouchMove);
+			this.dialogHeaderElement!.addEventListener('touchend', handleTouchEnd, {once: true});
+		}
+	};
+
+	private transferStyles() {
+		const declaredStyles = document.createElement('style');
+		declaredStyles.innerText = styles;
+		document.head.appendChild(declaredStyles);
+		if (!declaredStyles.sheet)
+			throw new Error("Could not create <style> element");
+	}
+
+	private getDataValue(path: Array<string>) {
+		return this.form.getDataValue(path);
+	}
+
+	private getActiveButton(path: Array<string>, action: string) : boolean {
+		const button = this.form.formset.buttons.find(button => isEqual(button.path, path));
+		return button?.element === document.activeElement;
+	}
+
+	public updateOperability() {
+		this.opener();
+	}
+}
+
+
 class DjangoForm {
 	public readonly formset: DjangoFormset;
 	public readonly element: HTMLFormElement;
@@ -908,6 +1052,7 @@ class DjangoForm {
 	private readonly errorPlaceholder: Element | null = null;
 	public readonly fieldGroups = Array<FieldGroup>(0);
 	public readonly hiddenInputFields = Array<HTMLInputElement>(0);
+	public readonly parentDialog: FormDialog | null;
 	public readonly isTransient: boolean;
 	public markedForRemoval = false;
 
@@ -922,6 +1067,8 @@ class DjangoForm {
 			this.errorList = placeholder.parentElement;
 			this.errorPlaceholder = this.errorList!.removeChild(placeholder);
 		}
+		const dialog = element.closest('dialog');
+		this.parentDialog = dialog ? new FormDialog(this, dialog) : null;
 		this.isTransient = this.element.getAttribute('df-transient') === 'true';
 		this.element.addEventListener('submit', this.handleSubmit);
 		this.element.addEventListener('reset', this.handleReset);
@@ -967,6 +1114,7 @@ class DjangoForm {
 	public updateOperability() {
 		this.fieldset?.updateOperability();
 		this.fieldGroups.forEach(fieldGroup => fieldGroup.updateOperability());
+		this.parentDialog?.updateOperability();
 	}
 
 	setSubmitted() {

formset/dialog.py
@@ -1,0 +1,28 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.forms import forms
+
+from formset.utils import FormMixin
+
+
+class DialogForm(FormMixin, forms.Form):
+    template_name = 'formset/default/form_dialog.html'
+    open_condition = None
+    title = None
+
+    def __init__(self, open_condition=None, title=None, **kwargs):
+        if open_condition:
+            self.open_condition = open_condition
+        if not self.open_condition:
+            raise ImproperlyConfigured("FormDialog requires an `open_condition`.")
+        if title:
+            self.title = title
+        super().__init__(**kwargs)
+
+    def get_context(self):
+        context = super().get_context()
+        context['form'].method = 'dialog'
+        context.update(
+            open_condition=self.open_condition,
+            title=self.title,
+        )
+        return context

formset/renderers/bootstrap.py
@@ -77,6 +77,9 @@ class FormRenderer(DefaultFormRenderer):
         )
         return context
 
+    def _amend_form_dialog(self, context):
+        return context
+
     _context_modifiers = dict(DefaultFormRenderer._context_modifiers, **{
         'django/forms/label.html': _amend_label,
         'django/forms/widgets/text.html': _amend_input,
@@ -100,6 +103,7 @@ class FormRenderer(DefaultFormRenderer):
         'formset/default/widgets/dual_selector.html': _amend_dual_selector,
         'formset/default/fieldset.html': _amend_fieldset,
         'formset/default/collection.html': _amend_collection,
+        'formset/default/form_dialog.html': _amend_form_dialog,
         'formset/default/widgets/richtextarea.html': _amend_input,
     })
 

formset/renderers/default.py
@@ -110,6 +110,9 @@ class FormRenderer(DjangoTemplates):
         )
         return context
 
+    def _amend_form_dialog(self, context):
+        return context
+
     _context_modifiers = {
         'django/forms/div.html': _amend_form,
         'django/forms/default.html': _amend_form,
@@ -120,6 +123,7 @@ class FormRenderer(DjangoTemplates):
         'formset/default/widgets/button.html': _amend_button,
         'formset/default/fieldset.html': _amend_fieldset,
         'formset/default/collection.html': _amend_collection,
+        'formset/default/form_dialog.html': _amend_form_dialog,
     }
 
     @classmethod

formset/static/formset/scss/bootstrap5-extra.scss
@@ -49,6 +49,9 @@ django-formset {
 		background-color: $modal-content-bg;
 		border: $modal-content-border-width solid $modal-content-border-color;
 		border-radius: $modal-content-border-radius;
+		box-shadow: $box-shadow;
+		z-index: 1;
+
 		.dialog-header {
 			display: flex;
 			flex-shrink: 0;
@@ -58,6 +61,11 @@ django-formset {
 			border-bottom: $modal-header-border-width solid $modal-header-border-color;
 			border-top-left-radius: $modal-content-inner-border-radius;
 			border-top-right-radius: $modal-content-inner-border-radius;
+
+			h3 {
+				margin: 0;
+			}
+
 			.modal-title {
 				line-height: $modal-title-line-height;
 				font-weight: bolder;

formset/templates/formset/default/form_dialog.html
@@ -1,0 +1,27 @@
+{% load i18n %}
+{% spaceless %}
+<dialog df-open-condition="{{ open_condition }}">
+	<form{% include "formset/form_attrs.html" %}></form>
+{% if form.title %}
+	<div class="dialog-header">
+		<h3>{{ form.title }}</h3>
+	</div>
+{% endif %}
+{% if form.fields %}
+	<div class="dialog-body">
+	{% for field in form %}
+		{% if field.is_hidden %}
+			{{ field }}
+		{% else %}
+			{% include "formset/default/field_group.html" %}
+		{% endif %}
+	{% endfor %}
+	{% if help_text %}{% include help_text_template %}{% endif %}
+	</div>
+{% endif %}
+	<div class="dialog-footer">
+		<button class="btn btn-outline-secondary" name="close" type="button" form="{{ form.form_id }}">{% translate "Close" %}</button>
+		<button class="btn btn-primary" name="save" value="save" type="submit" form="{{ form.form_id }}">{% translate "Submit" %}</button>
+	</div>
+</dialog>
+{% endspaceless %}

testapp/forms/profile.py
@@ -1,0 +1,41 @@
+from django.forms import fields, forms
+
+from formset.collection import FormCollection
+from formset.dialog import DialogForm
+from formset.fields import Button
+from formset.widgets import UploadedFileInput
+
+
+class CustomerForm(forms.Form):
+    username = fields.CharField()
+    edit_profile = Button(
+        label="Edit my Profile",
+        help_text = "Open the dialog to edit the profile",
+    )
+
+
+class ProfileForm(DialogForm):
+    title = "Edit Profile"
+    open_condition = 'customer.username == "admin" || customer.edit_profile:active'
+
+    full_name = fields.CharField(
+        label="First Name",
+        max_length=100,
+    )
+    email = fields.EmailField(
+        label="Email",
+    )
+    picture = fields.ImageField(
+        label="Picture",
+        required=False,
+        widget=UploadedFileInput,
+    )
+
+
+class ProfileCollection(FormCollection):
+    legend = "Collection with Dialog Form"
+    customer = CustomerForm()
+
+    extra_profile = Button()
+
+    profile = ProfileForm()

testapp/templates/bootstrap/base.html
@@ -22,7 +22,7 @@
 				<nav aria-label="breadcrumb mb-5">
 					<ol class="breadcrumb">
 						<li class="breadcrumb-item"><a href="/">{% include "testapp/house.svg" %}</a></li>
-						<li class="breadcrumb-item"><a href="/{{ framework }}">{{ framework.capitalize }}</a></li>
+						<li class="breadcrumb-item"><a href="/{{ framework }}/">{{ framework.capitalize }}</a></li>
 						<li class="breadcrumb-item"><strong>{{ leaf_name }}</strong></li>
 					</ol>
 				</nav>

commit 9f0aa353f5b31fcad6d6750c8eb1cf0c643e755e

fix regression introduced in af3b79873d

client/django-formset/DjangoFormset.ts
@@ -1785,7 +1785,7 @@ export class DjangoFormset {
 				continue;
 			setDataValue(this.data, form.getAbsPath(), Object.fromEntries(form.aggregateValues()));
 		}
-		// this.updateOperability();
+		this.updateOperability();
 	}
 
 	public updateOperability() {