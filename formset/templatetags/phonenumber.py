159bf91bcd11c87257654247be94861d859f1757
Jacob Rief <jacob.rief@gmail.com>
2024-01-16 23:50:40 +0100

    fail silently if package 'phonenumbers' is not installed

--- formset/templatetags/phonenumber.py
+++ formset/templatetags/phonenumber.py
@@ -1,13 +1,16 @@
 from django import template
-from phonenumbers import PhoneNumberFormat, format_number, parse
-
-
-def format_phonenumber(value, arg='international'):
-    """Formats a phonenumber according to the country's convenience."""
-    phone_number = parse(value, None)
-    num_format = PhoneNumberFormat.NATIONAL if arg == 'national' else PhoneNumberFormat.INTERNATIONAL
-    return format_number(phone_number, num_format)
-
+try:
+    from phonenumbers import PhoneNumberFormat, format_number, parse
+except ImportError:
+    def format_phonenumber(value, arg=None):
+        """As backup, render phonenumber in E.164 format."""
+        return value
+else:
+    def format_phonenumber(value, arg='international'):
+        """Formats a phonenumber according to the country's convenience."""
+        phone_number = parse(value, None)
+        num_format = PhoneNumberFormat.NATIONAL if arg == 'national' else PhoneNumberFormat.INTERNATIONAL
+        return format_number(phone_number, num_format)
 
 register = template.Library()
 register.filter('format_phonenumber', format_phonenumber)
