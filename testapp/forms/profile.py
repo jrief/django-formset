797246caa92551074fb4518887ceeaf7c7f716da
Jacob Rief <jacob.rief@gmail.com>
2024-01-16 23:51:41 +0100

    simple FormCollection to test against new FormDialog

--- testapp/forms/profile.py
+++ testapp/forms/profile.py
@@ -3,20 +3,22 @@ from django.forms import fields, forms
 from formset.collection import FormCollection
 from formset.dialog import DialogForm
 from formset.fields import Button
-from formset.widgets import UploadedFileInput
+from formset.renderers import ButtonVariant
+from formset.widgets import ButtonWidget, UploadedFileInput
 
 
-class CustomerForm(forms.Form):
+class UserNameForm(forms.Form):
     username = fields.CharField()
     edit_profile = Button(
         label="Edit my Profile",
-        help_text = "Open the dialog to edit the profile",
+        help_text="Open the dialog to edit the profile",
     )
 
 
 class ProfileForm(DialogForm):
     title = "Edit Profile"
-    open_condition = 'customer.username == "admin" || customer.edit_profile:active'
+    induce_open = 'customer.username == "admin" || extra_profile:active || customer.edit_profile:active'
+    induce_close = 'profile.dismiss:active || profile.submit:active'
 
     full_name = fields.CharField(
         label="First Name",
@@ -30,12 +32,28 @@ class ProfileForm(DialogForm):
         required=False,
         widget=UploadedFileInput,
     )
+    dismiss = Button(
+        label="Close",
+        widget=ButtonWidget(
+            action='activate("close")',
+        ),
+    )
+    submit = Button(
+        label="Save",
+        widget=ButtonWidget(
+            action='activate("save")',
+            button_variant=ButtonVariant.PRIMARY,
+        ),
+    )
 
 
 class ProfileCollection(FormCollection):
     legend = "Collection with Dialog Form"
-    customer = CustomerForm()
+    customer = UserNameForm()
 
-    extra_profile = Button()
+    extra_profile = Button(
+        label="Extra Profile Button",
+        help_text="Open the dialog to edit the profile",
+    )
 
     profile = ProfileForm()
