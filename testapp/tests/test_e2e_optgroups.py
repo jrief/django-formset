0a9c6a0cf4f3112ffd16a0cf3a8c2c87bb2ca084
Jacob Rief <jacob.rief@gmail.com>
2024-04-15 14:53:28 +0200

    wait between drags of options in E2E-test on sortable options

--- testapp/tests/test_e2e_optgroups.py
+++ testapp/tests/test_e2e_optgroups.py
@@ -213,7 +213,6 @@ def test_many_counties(page, form, viewname):
     assert selector.evaluate('elem => Array.from(elem.selectedOptions).map(o => o.value)') == expected
 
 
-@pytest.mark.xfail(reason="Playwright sometimes fails on GitHub actions")
 @pytest.mark.urls(__name__)
 @pytest.mark.parametrize('viewname', ['sortable_counties'])
 def test_sortable_counties(page, form, viewname):
@@ -234,6 +233,7 @@ def test_sortable_counties(page, form, viewname):
     expect(select_right.locator('optgroup option').last).to_have_text("Baca (CO)")
     before = [select_right.locator('optgroup option').nth(i).get_attribute('value') for i in range(5)]
     select_right.locator('optgroup').first.locator('option').last.drag_to(select_right.locator('optgroup').first.locator('option').first)
+    sleep(0.4)  # Sortable.js has a delay of 300ms
     select_right.locator('optgroup').first.locator('option').nth(1).drag_to(select_right.locator('optgroup').locator('option').last)
     sleep(0.4)  # Sortable.js has a delay of 300ms
     after = [select_right.locator('optgroup option').nth(i).get_attribute('value') for i in range(5)]
