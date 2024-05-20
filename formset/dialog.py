4e3cd1c73cd4a90f1f8582d2b34e4c7c4e318d3d
Jacob Rief <jacob.rief@gmail.com>
2024-05-20 10:47:19 +0200

    Create model dialog form

--- formset/dialog.py
+++ formset/dialog.py
@@ -1,26 +1,26 @@
-from django.forms import forms
+from django.forms.forms import BaseForm, DeclarativeFieldsMetaclass
+from django.forms.models import BaseModelForm, ModelFormMetaclass
 
 from formset.renderers import ButtonVariant
 from formset.utils import FormMixin
 from formset.widgets import Button
 
 
-class DialogForm(FormMixin, forms.BaseForm, metaclass=forms.DeclarativeFieldsMetaclass):
-    template_name = 'formset/default/form_dialog.html'
+class DialogFormMixin(FormMixin):
+    title = None
     is_modal = False
     induce_open, induce_close = None, None
     prologue, epilogue = None, None
-    title = None
 
-    def __init__(self, is_modal=False, induce_open=None, induce_close=None, title=None, **kwargs):
+    def __init__(self, title=None, is_modal=False, induce_open=None, induce_close=None, **kwargs):
+        if title:
+            self.title = title
         if is_modal:
             self.is_modal = True
         if induce_open:
             self.induce_open = induce_open
         if induce_close:
             self.induce_close = induce_close
-        if title:
-            self.title = title
         super().__init__(**kwargs)
 
     def get_context(self):
@@ -29,6 +29,14 @@ class DialogForm(FormMixin, forms.BaseForm, metaclass=forms.DeclarativeFieldsMet
         return context
 
 
+class DialogForm(DialogFormMixin, BaseForm, metaclass=DeclarativeFieldsMetaclass):
+    template_name = 'formset/default/form_dialog.html'
+
+
+class DialogModelForm(DialogFormMixin, BaseModelForm, metaclass=ModelFormMetaclass):
+    template_name = 'formset/default/form_dialog.html'
+
+
 ApplyButton = Button(action='activate("apply")', button_variant=ButtonVariant.PRIMARY)
-CancelButton = Button(action='activate("cancel")', button_variant=ButtonVariant.SECONDARY)
+CancelButton = Button(action='activate("close")', button_variant=ButtonVariant.SECONDARY)
 RevertButton = Button(action='activate("revert")', button_variant=ButtonVariant.DANGER)
