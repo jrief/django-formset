81e15e5d554a6da9efa57642800044d203fed136
Jacob Rief <jacob.rief@gmail.com>
2025-08-29 09:23:58 +0200

    fix #236: maxLength on slug field is handled properly
    fix #236: maxLength on slug field is handled properly

--- client/django-formset/DjangoSlug.ts
+++ client/django-formset/DjangoSlug.ts
@@ -10,12 +10,26 @@ export class DjangoSlugElement extends HTMLInputElement {
 		if (!(observedElement instanceof HTMLInputElement))
 			throw new Error(`Element <input name="${populateFrom}"> is missing on this form.`);
 		if (this.value === '') {
-			observedElement.addEventListener('input', (event: Event) => {
-				if (event.currentTarget instanceof HTMLInputElement) {
-					this.value = slug(event.currentTarget.value);
-					this.dispatchEvent(new Event('input'));
-				}
-			});
+			observedElement.addEventListener('input', this.setSlug);
 		}
 	}
+
+	disconnectedCallback() {
+		const populateFrom = this.getAttribute('populate-from') as string;
+		const observedElement = this.form?.elements.namedItem(populateFrom) as HTMLInputElement;
+		observedElement.removeEventListener('input', this.setSlug);
+	}
+
+	private setSlug = (event: Event) => {
+		const observedElement = event.currentTarget as HTMLInputElement;
+		const slugValue = slug(observedElement.value);
+		if (this.value !== slugValue) {
+			if (this.maxLength > 0) {
+				this.value = slugValue.substring(0, this.maxLength);
+			} else {
+				this.value = slugValue;
+			}
+			this.dispatchEvent(new Event('input'));
+		}
+	};
 }
