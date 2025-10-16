e1f4e58b8a2d01bd4c68eed0b710ef4bddc9ea04
Jacob Rief <jacob.rief@gmail.com>
2025-10-16 22:02:30 +0200

    escape element IDs in Calendar sheet

--- client/django-formset/Calendar.ts
+++ client/django-formset/Calendar.ts
@@ -891,8 +891,7 @@ export class CalendarSheet extends Widget {
 			for (let k = 0; k < sheet.cssRules.length; ++k) {
 				const cssRule = sheet.cssRules[k];
 				if (cssRule instanceof CSSStyleRule && cssRule.selectorText === `${this.baseSelector} .sheet-body ul:not(*)`) {
-					const elementId = this.settings.inputElement.id.replaceAll('.', '\\.');
-					const selectorText = `#${elementId} ~ ${this.baseSelector} .sheet-body ul:not(*)`;
+					const selectorText = `#${CSS.escape(this.settings.inputElement.id)} ~ ${this.baseSelector} .sheet-body ul:not(*)`;
 					const index = sheet.insertRule(`${selectorText}{${cssRule.style.cssText}}`, sheet.cssRules.length);
 					return sheet.cssRules[index] as CSSStyleRule;
 				}
