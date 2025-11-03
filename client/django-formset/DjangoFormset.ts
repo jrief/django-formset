60575b365e256c9785b7ab92dd1d7dd61d115828
Jacob Rief <jacob.rief@gmail.com>
2025-11-03 17:05:43 +0100

    fix df-require/disable/show/hide is target is UploadField

--- client/django-formset/DjangoFormset.ts
+++ client/django-formset/DjangoFormset.ts
@@ -35,6 +35,16 @@ if (style.sheet instanceof CSSStyleSheet) {
 	}, document.body]]);
 }
 
+function isTruthy(expression: any) : boolean {
+	// since empty arrays evaluate to true, implement separately to examine for existance
+	// when using the file upload widget. Check https://262.ecma-international.org/5.1/#sec-11.9.3
+	// for details.
+	if (Array.isArray(expression))
+		return expression.length !== 0 && expression.every(v => isTruthy(v));
+	if (isPlainObject(expression))
+		return !isEmpty(expression);
+	return Boolean(expression);
+}
 
 class BoundValue {
 	public readonly value: FieldValue;
@@ -265,13 +275,6 @@ class FieldGroup {
 	}
 
 	private evalVisibility(attribute: string, visible: boolean): Function|null {
-		function isTruthy(expression: any) : boolean {
-			// since empty arrays evaluate to true, implement separately to examine for existance
-			// when using the file upload widget. Check https://262.ecma-international.org/5.1/#sec-11.9.3
-			// for details.
-			return Array.isArray(expression) ? expression.length !== 0 : Boolean(expression);
-		}
-
 		const attrValue = this.fieldElements[0]?.getAttribute(attribute);
 		if (!isString(attrValue))
 			return null;
@@ -296,7 +299,7 @@ class FieldGroup {
 		try {
 			const evalExpression = new Function('return ' + parse(attrValue, {startRule: 'OperabilityExpression'}));
 			return () => {
-				const disable = evalExpression.call(this);
+				const disable = isTruthy(evalExpression.call(this));
 				this.fieldElements.forEach((elem, index) => elem.disabled = disable || this.initialDisabled[index]);
 			}
 		} catch (error) {
@@ -311,7 +314,7 @@ class FieldGroup {
 		try {
 			const evalExpression = new Function('return ' + parse(attrValue, {startRule: 'OperabilityExpression'}));
 			return () => {
-				const require = evalExpression.call(this);
+				const require = isTruthy(evalExpression.call(this));
 				this.fieldElements.forEach((elem, index) => elem.required = require && this.initialRequired[index]);
 				this.form.checkValidity();
 			}
