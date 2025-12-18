27617e02b759b50e4eaf30b2ba4d11400bae1f9b
Jacob Rief <jacob.rief@gmail.com>
2025-12-18 09:08:09 +0100

    Activator buttons now accept a value

--- formset/widgets/button.py
+++ formset/widgets/button.py
@@ -12,6 +12,7 @@ class Button(Widget):
     icon_path = None
     icon_char = None
     icon_left = None
+    value = None
 
     def __init__(
         self,
@@ -23,7 +24,8 @@ class Button(Widget):
         omit_restore=False,
         icon_path=None,
         icon_char=None,
-        icon_left=False
+        icon_left=False,
+        value=None,
     ):
         if action is not None:
             self.action = action
@@ -40,6 +42,8 @@ class Button(Widget):
         if icon_char:
             self.icon_char = icon_char
         self.icon_left = icon_left
+        if value is not None:
+            self.value = value
         super().__init__(attrs)
 
     def build_attrs(self, base_attrs, extra_attrs=None):
@@ -50,6 +54,8 @@ class Button(Widget):
             attrs['auto-disable'] = True
         if self.omit_restore:
             attrs['omit-restore'] = True
+        if self.value is not None:
+            attrs['value'] = self.value
         return attrs
 
     def get_context(self, name, value, attrs):
