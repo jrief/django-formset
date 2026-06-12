jrief/django-formset

commit 16fc6d07e5dbe2f0b4ff0f1ebb258f8d76f52b98

do not create aggregate data for transient sub-forms

client/django-formset/DjangoFormset.ts
@@ -2048,6 +2048,8 @@ export class DjangoFormset implements DjangoFormset {
 	public aggregateValues() {
 		this.data = {};
 		for (const form of this.forms) {
+			if (form.isTransient)
+				continue;
 			setDataValue(this.data, form.getAbsPath(), Object.fromEntries(form.aggregateValues()));
 		}
 	}

commit 744ed4ed7f78267e377b059f6261cb3037acfc29

revert regression introduced in 16fc6d07e5

client/django-formset/DjangoFormset.ts
@@ -2051,8 +2051,6 @@ export class DjangoFormset implements DjangoFormset {
 	public aggregateValues() {
 		this.data = {};
 		for (const form of this.forms) {
-			if (form.isTransient)
-				continue;
 			setDataValue(this.data, form.getAbsPath(), Object.fromEntries(form.aggregateValues()));
 		}
 	}