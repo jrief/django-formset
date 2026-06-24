55710b1e1b0916078c5762c0c69b9b5c61ffee47
Jacob Rief <jacob.rief@gmail.com>
2026-06-24 08:58:32 +0200

    Adopt position of form dialogs if their subtree change

--- client/django-formset/FormDialog.ts
+++ client/django-formset/FormDialog.ts
@@ -9,6 +9,7 @@ export abstract class FormDialogBase {
 	protected readonly element: HTMLDialogElement;
 	protected readonly formElement: HTMLFormElement;
 	private readonly dialogHeaderElement: HTMLElement|null;
+	private subtreeObserver: MutationObserver|null = null;
 	protected readonly isModal: boolean;
 	protected readonly induceOpen: Function;
 	protected readonly induceClose: Function;
@@ -18,7 +19,7 @@ export abstract class FormDialogBase {
 
 	constructor(element: HTMLDialogElement) {
 		this.element = element;
-		this.formElement = this.element.querySelector('form[method="dialog"]')!;
+		this.formElement = this.element.querySelector('form[method="dialog"]')! as HTMLFormElement;
 		if (!this.formElement)
 			throw new Error(`${this} requires child <form method="dialog">`);
 		this.dialogHeaderElement = this.element.querySelector('.dialog-header');
@@ -44,26 +45,37 @@ export abstract class FormDialogBase {
 	}
 
 	protected openDialog(button?: DjangoButton, ...args: any[]) {
-		const viewport = window.visualViewport;
-		if (this.element.open || !viewport)
+		if (this.element.open)
 			return;
 		if (this.isModal) {
 			this.element.showModal();
 		} else {
 			this.element.show();
-			if (this.dialogHeaderElement && !this.dialogRect) {
-				this.dialogRect = this.element.getBoundingClientRect();
-				this.dialogOffsetY = Math.max((viewport.height - this.dialogRect.height) / 2, 0);
-				this.element.style.transform = `translate(${this.dialogOffsetX}px, ${this.dialogOffsetY}px)`;
-				this.dialogHeaderElement.addEventListener('pointerdown', this.handlePointerDown);
-				this.dialogHeaderElement.addEventListener('touchstart', this.handlePointerDown);
-			}
+			this.positionDialogBox();
+			this.subtreeObserver = new MutationObserver(() => this.positionDialogBox());
+			this.subtreeObserver.observe(this.element, {childList: true, subtree: true});
 		}
 		this.element.addEventListener('close', () => this.closeDialog(), {once: true});
 	}
 
 	protected closeDialog(button?: DjangoButton, returnValue?: string) {
+		if (this.dialogHeaderElement) {
+			this.dialogHeaderElement.removeEventListener('pointerdown', this.handlePointerDown);
+			this.dialogHeaderElement.removeEventListener('touchstart', this.handlePointerDown);
+		}
+		this.subtreeObserver?.disconnect();
 		this.element.close(returnValue);
+	}
+
+	private positionDialogBox() {
+		const viewport = window.visualViewport;
+		if (this.dialogHeaderElement && viewport) {
+			this.dialogRect = this.element.getBoundingClientRect();
+			this.dialogOffsetY = Math.max((viewport.height - this.dialogRect.height) / 2, 0);
+			this.element.style.transform = `translate(${this.dialogOffsetX}px, ${this.dialogOffsetY}px)`;
+			this.dialogHeaderElement.addEventListener('pointerdown', this.handlePointerDown);
+			this.dialogHeaderElement.addEventListener('touchstart', this.handlePointerDown);
+		}
 	}
 
 	private handlePointerDown = (event: PointerEvent|TouchEvent) => {
