27a3abcbccf629068a08b07c0d1878541e32116a
Jacob Rief <jacob.rief@gmail.com>
2026-09-15 11:52:26 +0200

    add missing template for RTA's SimpleImageField

--- formset/static/formset/tiptap-extensions/simple_image.js
+++ formset/static/formset/tiptap-extensions/simple_image.js
@@ -31,7 +31,6 @@
 
 	image_to_document(elements) {
 		// richtext-map-to: SimpleImageDialogForm.image
-		console.log('image_to_document', elements);
 		const fileupload = JSON.parse(elements.image.dataset.fileupload);
 		return {
 			src: fileupload.download_url,
@@ -41,7 +40,6 @@
 
 	document_to_image(inputElement, attributes) {
 		// richtext-map-from: SimpleImageDialogForm.image
-		console.log('document_to_image', attributes, inputElement);
 		return {
 			dataset: {fileupload: JSON.stringify(attributes.dataset)},
 		};
